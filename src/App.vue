79c32da2622a294a6540678c5e932dae30733257
zhiyangcai <2313120974@qq.com>
2020-06-29 10:57:16 +0800

    添加404页面

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+<!--  <div id="app">-->
+<!--    <div id="nav">-->
+<!--      <router-link to="/">Home</router-link> |-->
+<!--      <router-link to="/about">About</router-link>-->
+<!--    </div>-->
+<!--    <router-view/>-->
+<!--  </div>-->
+  <div>
     <router-view/>
+    <hr/>
   </div>
 </template>
 
 <style lang="scss">
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,3 +16,6 @@ export default {
   }
 }
 </script>
+<style lang="scss" scoped>
+//@import "~@/assets/styles/text.scss";
+</style>
